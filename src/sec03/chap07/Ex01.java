8b530ae79e46df1b7b450bcb0350286110e6369e
eggtrt <eggtrt@gmail.com>
2024-03-23 22:31:05 +0900

    자료í형 String

--- src/sec03/chap07/Ex01.java
+++ src/sec03/chap07/Ex01.java
@@ -1,2 +1,7 @@
 package sec03.chap07;public class Ex01 {
+    public static void main(String[] args) {
+        //  ⭐️ 시스템의 printf 메소드 : String.format과 같은 형식으로 출력
+        //  줄바꿈을 하지 않으므로 직접 넣어줘야 함
+        System.out.printf("%s의 둘레는 반지름 X %d X %f입니다.%n", circle, two, PI);
+    }
 }
